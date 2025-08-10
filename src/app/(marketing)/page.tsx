0ada4e56aecabd14718e2808e3df74204a9aac60
Raphael <raphaelgarnier1997@gmail.com>
2025-08-10 16:46:01 +0200

    style(homapage): - Adding shadcn ui to the project - Use button component from shadcn ui in the homepage

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -20,8 +22,16 @@ export default function Home() {
           Use PetSoft to easily keep track of pets under your care. Get lifetime acces for
           $299.
         </p>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get started</Link>
+          </Button>
+
+          <Button asChild variant="secondary">
+            <Link href="/signup">Log in</Link>
+          </Button>
+        </div>
       </div>
-      <div className="mt-10"></div>
     </main>
   );
 }
