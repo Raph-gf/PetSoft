6b4ff83b4530426bb1b2fb25c6ddb1036fc27886
Raphael <raphaelgarnier1997@gmail.com>
2025-08-10 16:22:59 +0200

    feat(homepage): - Creating & styling the homepage with tailwind css

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,27 @@
+import Logo from "@/components/logo";
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Home Page</main>;
+  return (
+    <main className="flex flex-col items-center justify-center gap-10  xl:flex-row bg-[#5DC9A8] min-h-screen">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Preview of PetSoft"
+        width={519}
+        height={472}
+        priority
+      />
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your pet <span className="font-extrabold">daycare with ease</span>{" "}
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          Use PetSoft to easily keep track of pets under your care. Get lifetime acces for
+          $299.
+        </p>
+      </div>
+      <div className="mt-10"></div>
+    </main>
+  );
 }
