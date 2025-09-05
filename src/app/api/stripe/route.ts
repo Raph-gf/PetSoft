e28d85aea932aecea7fff39c788d71282a10dd75
Raphael <raphaelgarnier1997@gmail.com>
2025-09-05 18:15:08 +0200

    Stripe Weebhooks setup && installing ngrok for developpement purpose

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,4 +1,7 @@
+import { NextResponse } from "next/server";
+
 export async function POST(request: Request) {
   const body = await request.text();
   console.log(body);
+  return NextResponse.json(null, { status: 200 });
 }
